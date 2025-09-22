a338b09121b47e29f1acabcc5800b40fae838d17
pritom-bod <pritomsarker406@gmail.com>
2025-09-22 14:10:48 +0600

    fix

--- analyzer/settings.py
+++ analyzer/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-your-secret-key-here')  # Set in Render
 DEBUG = os.getenv('DEBUG', 'True') == 'True'  # False in production
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1','v-comparison-v2-backend.onrender.com','.onrender.com', ]  # Render domain
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','cv-comparison-v2-backend.onrender.com','.onrender.com', ]  # Render domain
 
 # Installed apps
 INSTALLED_APPS = [
